263660cfdda7650fe95ee9739feeef1900da9412
kganesh-tech <k.ganesh6286@gmail.com>
2026-09-17 19:28:37 +0530

    Update nodemailer.js

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service : "gmail",
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
@@ -12,20 +12,24 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-const sendEmail = async (userEmail , resetLink) => {
+const sendEmail = async (userEmail, resetLink) => {
     try {
         const info = await transporter.sendMail({
-            from : process.env.EMAIL_USER,
-            to : userEmail,
-            subject : "GANTHRO password reset",
-            text : `Reset your password here: ${resetLink}`
-
+            from: process.env.EMAIL_USER,
+            to: userEmail,
+            subject: "GANTHRO password reset",
+            text: `Reset your password here: ${resetLink}`
         });
+
         console.log("EMAIL sent successfully");
         console.log(info.response);
-    }catch (error) {
-        console.log("ERROR sending email:" , error);
 
-    };
+        return true;
+
+    } catch (error) {
+        console.log("ERROR sending email:", error);
+        throw error;
+    }
 };
+
 module.exports = sendEmail;
